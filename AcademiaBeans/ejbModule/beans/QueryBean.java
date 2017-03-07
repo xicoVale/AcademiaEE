9bbeb0c390565fefa6c5d863ffa1a560776b245b
Altran <Altran@LPT-FA3290>
2017-03-07 17:27:27 +0000

    Implemented userExists and registerUser

--- AcademiaBeans/ejbModule/beans/QueryBean.java
+++ AcademiaBeans/ejbModule/beans/QueryBean.java
@@ -1,18 +1,56 @@
 package beans;
 
+import java.security.SecureRandom;
+import java.sql.ResultSet;
+import java.sql.Statement;
+import java.util.Random;
+
 import javax.ejb.Stateless;
+import javax.inject.Inject;
+import javax.persistence.EntityManager;
 
 /**
  * Session Bean implementation class QueryBean
  */
 @Stateless
 public class QueryBean implements QueryBeanRemote, QueryBeanLocal {
-
-    /**
-     * Default constructor. 
-     */
+	
+//	static Statement stmt = null;
+	@Inject EntityManager em;
+	
+	
+  
+//    Default constructor.      
     public QueryBean() {
         // TODO Auto-generated constructor stub
+    	
     }
+
+	@Override
+	public Boolean userExists(String userName) {
+		Boolean exist;
+		exist=!em.createQuery("SELECT userName FROM users"
+				+ " WHERE USERNAME = "+ userName+";").getResultList().isEmpty();
+	
+		return exist;
+	}
+
+	@Override
+	public void registerUser(String userName, String password) {
+		
+		final Random r = new SecureRandom();
+		byte[] salt = new byte[32];
+		r.nextBytes(salt);
+		String saltString = salt.toString();
+		String code = ((Integer)(password+saltString).hashCode()).toString();
+		
+		em.createQuery("INSERT INTO users (USERNAME,PASSWORD,SALT) VALUES ('"+userName+"','"+code+"','"+saltString+"'");		
+		
+		
+	}
+
+
+
+
 
 }
--- AcademiaBeans/ejbModule/beans/QueryBeanLocal.java
+++ AcademiaBeans/ejbModule/beans/QueryBeanLocal.java
@@ -5,6 +5,9 @@ import javax.ejb.Local;
 @Local
 public interface QueryBeanLocal {
 
-	public Boolean existUser{};
+
+	public Boolean userExists(String userName);
+	public void registerUser(String userName, String password);
+	public Boolean
 	
 }
