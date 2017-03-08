c0c80f4b582069e9bd89d0d30e0d9f5d28ab29a3
Altran <Altran@LPT-FA3290>
2017-03-08 18:03:15 +0000

    Created Questions

--- AcademiaInqueritos/src/beans/InqueriesBean.java
+++ AcademiaInqueritos/src/beans/InqueriesBean.java
@@ -1,6 +1,7 @@
 package beans;
 
 import java.io.Serializable;
+import java.util.ArrayList;
 import java.util.Date;
 
 import javax.annotation.ManagedBean;
@@ -8,7 +9,9 @@ import javax.faces.bean.SessionScoped;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 
+import entities.Answers;
 import entities.Inqueries;
+import entities.Questions;
 
 /**
  * Session Bean implementation class InqueriesBean
@@ -24,7 +27,9 @@ public class InqueriesBean implements Serializable{
 	private EntityManager em;
 	
 	
-	Inqueries inquery = new Inqueries();
+	private Inqueries inquery = new Inqueries();
+	private Questions question = new Questions();
+	private ArrayList<Answers> answers= new ArrayList<Answers>();
 	
     /**
      * Default constructor. 
@@ -33,50 +38,80 @@ public class InqueriesBean implements Serializable{
         
     }
 
-    private Boolean verifySomeNameAndSomeDate(Inqueries inquery){
-    	this.inquery = em.find(Inqueries.class, inquery);
-		
-		if(this.inquery.getTitle().equals(inquery.getTitle())){
-		
-		Date dateEndA = inquery.getEndDate();
-		Date dateStartA= inquery.getStartDate();
-		Date dateEndB= this.inquery.getEndDate();
-		Date dateStartB= this.inquery.getStartDate();
-		
-		if((dateEndB.compareTo(dateEndA)<=0 && dateEndB.compareTo(dateStartA)>0)){
+	private Boolean verifySomeNameAndSomeDate(Inqueries inquery) {
+		this.inquery = em.find(Inqueries.class, inquery);
+
+		if (this.inquery.getTitle().equals(inquery.getTitle())) {
+
+			Date dateEndA = inquery.getEndDate();
+			Date dateStartA = inquery.getStartDate();
+			Date dateEndB = this.inquery.getEndDate();
+			Date dateStartB = this.inquery.getStartDate();
+
+			if ((dateEndB.compareTo(dateEndA) <= 0 && dateEndB.compareTo(dateStartA) > 0)) {
+				return true;
+
+			} else if (dateStartB.compareTo(dateStartA) >= 0 && dateStartB.compareTo(dateEndA) < 0) {
+				return true;
+
+			} else if (dateStartB.compareTo(dateStartA) <= 0 && dateEndB.compareTo(dateEndA) >= 0) {
+				return true;
+
+			} else if (dateStartA.compareTo(dateStartB) <= 0 && dateEndA.compareTo(dateEndB) >= 0) {
+				return true;
+
+			} else {
+				return false;
+			}
+
+		} else {
 			return false;
-			
-		}else if(dateStartB.compareTo(dateStartA)>=0 && dateStartB.compareTo(dateEndA)<0){
-			return false;
-			
-		}else if(dateStartB.compareTo(dateStartA)<=0 && dateEndB.compareTo(dateEndA)>=0){
-			return false;
-			
-		}else if(dateStartA.compareTo(dateStartB)<=0 && dateEndA.compareTo(dateEndB)>=0){
-			return false;
-			
-		}else{
-			return true;
 		}
-				
-	
-		}
-		else{
-			return true;
-		}
-		
-				
-    }
+
+	}
     
 	public String registerInquerie(Inqueries inquery) {
 		if(verifySomeNameAndSomeDate(inquery)){
 			
-			em.persist(inquery);
-			return "success";
+			return "someNameAndCoincidenceDates";
+			
 			
 		}else{
-			return "someNameAndCoincidenceDates";
+			em.persist(inquery);
+			return "success";
 		}
 	}
+	
+		
+	public Inqueries getInquery() {
+		return inquery;
+	}
+
+	public void setInquery(Inqueries inquery) {
+		this.inquery = inquery;
+	}
+
+//	QUESTIONS
+	
+	public String registerQuestion(Questions question){
+		em.persist(question);
+		return "success";
+		
+	}
+
+	public Questions getQuestion() {
+		return question;
+	}
+
+	public void setQuestion(Questions question) {
+		this.question = question;
+	}
+	
+// ANSWERS
+	
+	
+	
+	
+	
     
 }
