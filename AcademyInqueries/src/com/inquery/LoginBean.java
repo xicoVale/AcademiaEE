494134a1e2915f994ab8f8fc90ba0ed599e10433
vanessasardinha <vanessamoraissardinha@gmail.com>
2017-03-08 17:14:55 +0000

    FIX

---
+++ AcademyInqueries/src/com/inquery/LoginBean.java
@@ -0,0 +1,28 @@
+/**
+ * LoginBean.java
+ * 
+ */
+
+package com.inquery;
+
+public class LoginBean {
+	private String name;
+	private String password;
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(final String name) {
+		this.name = name;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(final String password) {
+		this.password = password;
+	}
+	
+}
---
+++ AcademyInqueries/src/com/inquery/VerifyUserBean.java
@@ -0,0 +1,20 @@
+package com.inquery;
+
+public class VerifyUserBean {
+	
+	private String userID;
+	
+	public VerifyUserBean() {
+	}
+
+	public String getUserID() {
+		return userID;
+	}
+
+	public void setUserID(String userID) {
+		this.userID = userID;
+	}
+
+
+}
+
