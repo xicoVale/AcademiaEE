20311efa30f9b39c8265b96659291086af6cdb62
Francisco Vale <xico.vale@hotmail.com>
2017-03-08 12:18:25 +0000

    Created exceptions for Users existing or not in the database

---
+++ AcademiaBeans/ejbModule/exceptions/UserNotExistsException.java
@@ -0,0 +1,13 @@
+package exceptions;
+
+public class UserNotExistsException extends Exception {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -7070380866710946732L;
+
+	public UserNotExistsException() {
+		super();
+	}
+}
